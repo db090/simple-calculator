417b04172bbf349ea595cbac3195b2dc9163ccde
Daniyal <dshamraiz61@gmail.com>
2024-03-23 01:47:46 +0500

    pushing code to github

---
+++ index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        message: "enter first number",
+        name: "firstNumber",
+    },
+    {
+        type: "number",
+        message: "enter first number",
+        name: "secondNumber",
+    },
+    {
+        type: "list",
+        message: "select one of the operator to perform an operation",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+if (answer.operator === "addition") {
+    console.log(`The sum of these two values is = ${answer.firstNumber + answer.secondNumber}`);
+}
+else if (answer.operator === "subtraction") {
+    console.log(`The sub of these two values is = ${answer.firstNumber - answer.secondNumber}`);
+}
+else if (answer.operator === "multiplication") {
+    console.log(`The multiplication of these two values is = ${answer.firstNumber * answer.secondNumber}`);
+}
+else if (answer.operator === "division") {
+    console.log(`The division of these two values is = ${answer.firstNumber / answer.secondNumber}`);
+}
+else {
+    console.log("please selcet a valid operator");
+}
